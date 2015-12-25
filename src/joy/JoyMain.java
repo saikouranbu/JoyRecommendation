3f44c86407ac01fa14833e9ba361db1353cdcdb9
tsukada <tsukada@cps.im.dendai.ac.jp>
2015-12-25 20:00:28 +0900

    historyRun試行錯誤途中

--- src/joy/JoyMain.java
+++ src/joy/JoyMain.java
@@ -3,8 +3,9 @@ package joy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
+
+import org.openqa.selenium.WebElement;
 
 public class JoyMain {
 	public static void main(String[] args) {
@@ -24,11 +25,6 @@ public class JoyMain {
 			// https://www.joysound.com/utasuki/userpage/index.htm?usr=31355ec2a6046f5b67aa547bed58275a2a47
 			pageUrl1 = reader.readLine();
 			id1 = pageUrl1.substring(56, pageUrl1.length()); // URLからユーザIDを抽出
-			//System.out.println(id1);
-			/*
-			 * WebPage web1 = new WebPage(new URL(pageUrl1)); //
-			 * web1.setExpression("/utasuki/index.htm"); web1.run();
-			 */
 			historyRun(id1);
 
 			System.out.print("相手のページのURLを入力してください：");
@@ -36,28 +32,72 @@ public class JoyMain {
 			// https://www.joysound.com/utasuki/userpage/index.htm?usr=aaadbcf6d1d012bcc59cf1c3f92c0d53414d
 			pageUrl2 = reader.readLine();
 			id2 = pageUrl2.substring(56, pageUrl2.length()); // URLからユーザIDを抽出
-			//System.out.println(id2);
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
 	}
 
-	public void historyRun(String id){
+	public void historyRun(String id) {
+		Thread th = new Thread();
 		String urlFirst = "https://www.joysound.com/utasuki/userpage/history/index.htm?usr=";
 		StringBuilder urlBuilder = new StringBuilder();
 		urlBuilder.append(urlFirst);
 		urlBuilder.append(id);
 		String url = urlBuilder.toString();
 
+		PhantomJs ph = PhantomJs.create();
+		ArrayList<WebElement> historyList;
+		ph.getDriver().get(url);
 		try {
-			WebPage web = new WebPage(new URL(url));
-			web.setExpression("span");
-			web.historyRun();
-		} catch (MalformedURLException e) {
+			th.sleep(500);
+			String historyStr = ph.getDriver().findElementByTagName("em")
+					.getText();
+			String historyNum = historyStr
+					.substring(0, historyStr.length() - 1);
+			int historyInt = Integer.parseInt(historyNum);
+
+						String songwordstitle, name, artist, relation;
+
+			for (int i = 0; i < 20; i++) {
+				th.sleep(500);
+
+				//System.out.println("0");
+				historyList = (ArrayList<WebElement>) ph.getDriver()
+						.findElementsByClassName("usk-block-link");
+
+				//System.out.println("1");
+				WebElement e = historyList.get(i);
+				//System.out.println("2");
+				e.click();
+				//System.out.println("3");
+
+				songwordstitle = ph
+						.getDriver()
+						.findElementByXPath(
+								"//div[@class='jp-cmp-song-words-title ng-binding']")
+						.getText();
+				name = songwordstitle.substring(0, songwordstitle.length() - 3);
+				name = name.substring(1, name.length() - 1);
+				artist = ph.getDriver()
+						.findElementByClassName("jp-cmp-table-column-001")
+						.getText();
+				relation = ph
+						.getDriver()
+						.findElementByXPath(
+								"//div[@data-ng-repeat='tieup in detail.tieupList']")
+						.getText();
+				System.out.println(name + "," + artist + "," + relation);
+				ph.getDriver().navigate().back();
+				//System.out.println(".");
+				// System.out.println(ph.getDriver().getPageSource());
+			}
+
+		} catch (InterruptedException e1) {
 			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-			return;
+			e1.printStackTrace();
+		} finally {
+			ph.quit();
 		}
 	}
 }
--- src/joy/PhantomJs.java
+++ src/joy/PhantomJs.java
@@ -13,48 +13,60 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
  * A little PhantomJS Service Wrapper
  */
 public class PhantomJs {
+	private static PhantomJSDriver driver;
+	private static PhantomJs phantomJs;
+
 	public static void main(String[] args) {
-        PhantomJs ph = create();
-        ArrayList<WebElement> list;
-        ph.driver.get("https://www.joysound.com/utasuki/userpage/history/index.htm?usr=aaadbcf6d1d012bcc59cf1c3f92c0d53414d");
-        int historyInt;
-        System.out.println(ph.driver.findElementByTagName("em").getText());
-        //System.out.println(ph.driver.getPageSource());
-        /*list = (ArrayList<WebElement>) ph.driver.findElementsByClassName("usk-block-link");
-        for(WebElement e : list){
-        	System.out.println(e.getText());
-        }*/
-        ph.driver.quit();
-        
-        //System.out.println(element.getText());
-        //System.out.println("Page title is: " + driver.getTitle());
-    }
-    public static PhantomJs create(PhantomJSDriver driver) {
-        PhantomJs phantomJs;
+		PhantomJs ph = create(new PhantomJSDriver());
+		ArrayList<WebElement> list;
+		ph.driver
+				.get("https://www.joysound.com/utasuki/userpage/history/index.htm?usr=aaadbcf6d1d012bcc59cf1c3f92c0d53414d");
+		String historyStr = ph.driver.findElementByTagName("em").getText();
+		String historyNum = historyStr.substring(0, historyStr.length() - 1);
+		int historyInt = Integer.parseInt(historyNum);
+		System.out.println(historyInt);
+		// System.out.println(ph.driver.getPageSource());
+		/*
+		 * list = (ArrayList<WebElement>)
+		 * ph.driver.findElementsByClassName("usk-block-link"); for(WebElement e
+		 * : list){ System.out.println(e.getText()); }
+		 */
+		ph.driver.quit();
 
-        phantomJs = new PhantomJs();
+		// System.out.println(element.getText());
+		// System.out.println("Page title is: " + driver.getTitle());
+	}
 
-        phantomJs.driver = driver;
+	public static PhantomJs create(PhantomJSDriver driver) {
+		phantomJs = new PhantomJs();
 
-        return phantomJs;
-    }
+		phantomJs.driver = driver;
 
-    public static PhantomJs create() {
-        return create(new PhantomJSDriver());
-    }
+		return phantomJs;
+	}
 
-    private PhantomJSDriver driver;
+	public static PhantomJs create() {
+		return create(new PhantomJSDriver());
+	}
 
-    public void changeBrowserDimensions(int width, int height) {
-        driver.manage().window().setSize(new Dimension(width, height));
-    }
+	public PhantomJSDriver getDriver(){
+		return driver;
+	}
 
-    public String executeScript(String script, String... args){
-        return String.valueOf(driver.executePhantomJS(script, args));
-    }
+	public void quit(){
+		driver.quit();
+	}
 
-    public File takeScreenshot(URL url) {
-        driver.get(url.toString());
-        return driver.getScreenshotAs(OutputType.FILE);
-    }
+	public void changeBrowserDimensions(int width, int height) {
+		driver.manage().window().setSize(new Dimension(width, height));
+	}
+
+	public String executeScript(String script, String... args) {
+		return String.valueOf(driver.executePhantomJS(script, args));
+	}
+
+	public File takeScreenshot(URL url) {
+		driver.get(url.toString());
+		return driver.getScreenshotAs(OutputType.FILE);
+	}
 }
